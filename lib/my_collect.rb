9d7d51a87ae139c6f9b2d8efeb3bae00ca2a4d0d
melia miller <meliajm@gmail.com>
2019-12-11 00:59:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,12 +8,3 @@ def my_collect(array)
   collection
 end
 
-# def hello(array)
-#   i = 0
-#   collection = []
-#   while i < array.length
-#     collection << yield(array[i])
-#     i += 1
-#   end
-#   collection
-# end
