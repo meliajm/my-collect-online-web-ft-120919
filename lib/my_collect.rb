ef2abfd0532aaac990b556426acb6ca93ce9f20a
melia miller <meliajm@gmail.com>
2019-12-11 00:58:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,19 @@
 def my_collect(array)
   i = 0 
   collection = []
-  while i < array.length do 
-    yield collection << array[i]
+  while i < array.length  
+    collection << yield(array[i])
     i += 1
   end 
   collection
 end
 
+# def hello(array)
+#   i = 0
+#   collection = []
+#   while i < array.length
+#     collection << yield(array[i])
+#     i += 1
+#   end
+#   collection
+# end
